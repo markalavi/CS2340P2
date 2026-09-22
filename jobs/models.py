c2be16602948e10f475e574419c2f422c7f01f96
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-09-21 21:40:29 -0400

    Add application.Application and jobs.Job models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Job(models.Model):
+    """Represents a job listing posted by a recruiter on the site."""
+    id = models.AutoField(primary_key=True)
+
+    # The recruiter that posted the job listing
+    recruiter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # The job description
+    description = models.TextField(max_length=1024)
+
+    # Annualized salary standardized in cents.
+    annual_salary_in_cents = models.IntegerField()
