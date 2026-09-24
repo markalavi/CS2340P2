3cc0341ea0f7df1efe03563dbb7f26583c224f70
Parker Melling <pmelling3@gatech.edu>
2026-09-24 17:33:20 -0400

    Format Fixes

--- recommendations/scoring.py
+++ recommendations/scoring.py
@@ -1,3 +1,3 @@
 # Returns a candidate's score (ranges from 0.0 to 1.0)
-def candidate_score(required_skills : set, candidate_skills : set) -> float:
-    return len(required_skills.intersection(candidate_skills)) / len(required_skills)
+def candidate_score(required_skills: set, candidate_skills: set) -> float:
+	return len(required_skills.intersection(candidate_skills)) / len(required_skills)
