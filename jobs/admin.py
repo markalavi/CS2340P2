7604c817976294f7a5b8f8ce921467fe042e9773
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-09-25 19:43:57 -0400

    add: Create urls and views for recruiter-facing job-related info

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Job
 
-admin.site.register(Job)
+
+class CustomJobAdmin(admin.ModelAdmin):
+    """Admin registration for jobs."""
+
+    list_display = [
+        'id',
+        'recruiter',
+        'title',
+        'location',
+        'description',
+        'annual_salary_in_cents',
+        'company',
+        'created_at',
+    ]
+
+admin.site.register(Job, CustomJobAdmin)
--- jobs/urls.py
+++ jobs/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from django.urls import path
+
+from jobs import views
+
+app_name = 'jobs'
+
+urlpatterns = [
+    path('', views.job_list, name='list'),
+    path('new/', views.job_create, name='create'),
+    path('browse/', views.job_browse, name='browse'),
+    path('<int:pk>/', views.job_detail, name='detail'),
+    path('<int:pk>/edit/', views.job_edit, name='edit'),
+]
